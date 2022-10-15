523ca871276baaf8fb6f1305b82432b39120e61c
Seb <63566437+roadhog71@users.noreply.github.com>
2022-10-14 21:17:29 -0300

    models added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from msilib.schema import Class
 from unittest.util import _MAX_LENGTH
 from django.db import models
@@ -14,3 +15,42 @@ class Customer(models.Model):
          
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+class Item(models.Model):
+    id= models.BigAutoField(primary_key= True)
+    item= models.CharField(max_length= 64)
+    price= models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'items'
+        managed= True
+         
+    def __str__(self) -> str:
+        return f"{self.item} ${self.price}"
+
+class Order(models.Model):
+    id= models.BigAutoField(primary_key= True)
+    date= models.DateField()
+    customer= models.ForeignKey(Customer, on_delete=models.CASCADE)
+    status= models.SmallIntegerField(default=1)
+
+    class Meta:
+        db_table = 'orders'
+        managed= True
+         
+    def __str__(self) -> str:
+        return f"{self.date} {self.customer} {self.status}"
+
+class OrderItem(models.Model):
+    id= models.BigAutoField(primary_key= True)
+    order= models.ForeignKey(Order, on_delete=models.CASCADE)
+    item= models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity= models.DecimalField()
+    price= models.DecimalField()
+
+    class Meta:
+        db_table = 'order_items'
+        managed= True
+         
+    def __str__(self) -> str:
+        return f"{self.item} {self.quantity} {self.price}"
